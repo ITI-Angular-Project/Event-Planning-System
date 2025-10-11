570c7e2affb7531dbff9b13a55b3e661a4380b6c
Ahmed Alaa <107187857+Ahmedalla1144@users.noreply.github.com>
2025-10-11 16:58:27 +0300

    Setting up project layout (#7)
    * init-repo
    
    * init repo
    
    * Update README.md to enhance project structure clarity and documentation
    
    * Update README.md
    
    * feat: integrate Tailwind CSS for styling and implement theme toggle functionality
    
    - Added Tailwind CSS and PostCSS dependencies in package.json
    - Created a ThemeService for managing light/dark themes
    - Implemented theme toggle button in app.html
    - Updated app.ts to utilize ThemeService and manage theme state
    - Configured Tailwind CSS in styles.css and added custom styles
    - Created a PostCSS configuration file for Tailwind
    - Added unit tests for ThemeService
    - Seeded local storage with initial data for users, events, guests, tasks, expenses, and feedback
    - Configured Tailwind CSS with custom color themes in tailwind.config.js

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
+import { ThemeService } from './core/services/themeService/theme-service';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Event-Planning-System');
+
+  constructor(public theme: ThemeService) {}
+
+  get themeSignal() {
+    return this.theme.theme;
+  }
 }
