ae7064b3b65202237669a9a41fa74ef05ab093a0
Ahmed Alaa <107187857+Ahmedalla1144@users.noreply.github.com>
2025-10-12 04:15:40 +0300

    Dashboard layout and data.json (#9)
    * init-repo
    
    * init repo
    
    * Update README.md to enhance project structure clarity and documentation
    
    * Update README.md
    
    * feat: integrate Tailwind CSS for styling and implement theme toggle functionality
    
    - Added Tailwind CSS and PostCSS dependencies in package.json
    - Created a ThemeService for managing light/dark themes
    - Implemented theme toggle button in app.html
    - Updated app.ts to utilize ThemeService and manage theme state
    - Configured Tailwind CSS in styles.css and added custom styles
    - Created a PostCSS configuration file for Tailwind
    - Added unit tests for ThemeService
    - Seeded local storage with initial data for users, events, guests, tasks, expenses, and feedback
    - Configured Tailwind CSS with custom color themes in tailwind.config.js
    
    * adding public and dashboard layouts.
    making dashboard routs.
    create not found component.
    
    * implement dashboard and public layouts with initial components and tests
    
    * finish styling not-found page.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { NotFound } from './pages/not-found/not-found';
+import { PublicLayout } from './layouts/public-layout/public-layout';
+import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+import { HomeDashboard } from './pages/dashboard/home/home-dashboard';
+import { EventsList } from './pages/dashboard/events/events-list/events-list';
+import { EventDetails } from './pages/dashboard/events/event-details/event-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: PublicLayout,
+    children: [],
+  },
+  {
+    path: 'dashboard',
+    component: DashboardLayout,
+    children: [
+      {
+        path: '',
+        component: HomeDashboard,
+      },
+      {
+        path: 'events',
+        component: EventsList,
+      },
+      {
+        path: 'events/:id',
+        component: EventDetails,
+      },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFound,
+  },
+];
---
+++ src/app/layouts/dashboard-layout/dashboard-layout.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HomeDashboard } from '../../pages/dashboard/home/home-dashboard';
+
+@Component({
+  selector: 'app-dashboard-layout',
+  imports: [HomeDashboard],
+  templateUrl: './dashboard-layout.html',
+  styleUrl: './dashboard-layout.css'
+})
+export class DashboardLayout {
+
+}
