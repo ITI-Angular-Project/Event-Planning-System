2f422105b60fa8d0cb6bda47015f2f2ac40c65b4
Ahmed Alaa <107187857+Ahmedalla1144@users.noreply.github.com>
2025-10-14 12:17:08 +0300

    login and register (#14)
    Co-authored-by: Bassel Essam <baselessam550@gmail.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,34 @@
 import { Routes } from '@angular/router';
-import { NotFound } from './pages/not-found/not-found';
+
+// Layouts
 import { PublicLayout } from './layouts/public-layout/public-layout';
 import { DashboardLayout } from './layouts/dashboard-layout/dashboard-layout';
+
+// Pages - Dashboard
 import { HomeDashboard } from './pages/dashboard/home/home-dashboard';
 import { EventsList } from './pages/dashboard/events/events-list/events-list';
 import { EventDetails } from './pages/dashboard/events/event-details/event-details';
 
+// Pages - log-reg
+import { Login } from './pages/log-reg/login/login';
+import { Register } from './pages/log-reg/register/register';
+
+// Other Pages
+import { NotFound } from './pages/not-found/not-found';
+
 export const routes: Routes = [
+
   {
     path: '',
     component: PublicLayout,
-    children: [],
+    children: [
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+    ],
   },
+
+
+  // Dashboard Layout
   {
     path: 'dashboard',
     component: DashboardLayout,
@@ -30,6 +47,8 @@ export const routes: Routes = [
       },
     ],
   },
+
+  // Not Found
   {
     path: '**',
     component: NotFound,
