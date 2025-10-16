f3d3a1d1c6789799c14bae85fcf0ea5084535c4d
Bassel Essam <baselessam550@gmail.com>
2025-10-16 18:16:18 +0300

    fixing confliect

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,17 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Loader } from "../../pages/componants/loader/loader";
 import { Navbar } from "../../pages/componants/navbar/navbar";
-import { Home } from "../../pages/componants/home/home";
-import { RolesSection } from "../../pages/componants/roles-section/roles-section";
-import { EventVideoSection } from "../../pages/componants/event-video-section/event-video-section";
-import { Speakers } from "../../pages/componants/speakers/speakers";
 import { Footer } from "../../pages/componants/footer/footer";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [CommonModule, Loader, Navbar, Home, RolesSection, EventVideoSection, Speakers, Footer],
+  imports: [CommonModule, Navbar, Footer, RouterOutlet],
   templateUrl: './public-layout.html',
   styleUrls: ['./public-layout.css']
 })
