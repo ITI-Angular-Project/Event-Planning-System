8e80d0ce8519f3d303581be4891f461fd38a4cd5
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-10-16 16:34:22 +0300

    refactor: remove unused RouterOutlet import from public layout and RouterLinkActive from login component

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,6 +1,5 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 import { Loader } from "../../pages/componants/loader/loader";
 import { Navbar } from "../../pages/componants/navbar/navbar";
 import { Home } from "../../pages/componants/home/home";
@@ -12,16 +11,15 @@ import { Footer } from "../../pages/componants/footer/footer";
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, Loader, Navbar, Home, RolesSection, EventVideoSection, Speakers, Footer],
+  imports: [CommonModule, Loader, Navbar, Home, RolesSection, EventVideoSection, Speakers, Footer],
   templateUrl: './public-layout.html',
   styleUrls: ['./public-layout.css']
 })
 export class PublicLayout {
   loaderLoading = signal(true);
-  // protected readonly title = ('day3');
   constructor() {
     setTimeout(() => {
       this.loaderLoading.set(false);
-    }, 2500); // match loader delay
+    }, 2500);
   }
 }
