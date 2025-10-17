75a865ebea67be0e5119fa41459d07d2036b12ad
Ahmed Alaa <ahmed.alla56756@gmail.com>
2025-10-17 03:02:47 +0300

    refactor: remove LoggedUser interface and update related components to use User interface; enhance sidebar and profile components with mobile support and improved user handling

--- src/app/layouts/dashboard-layout/dashboard-layout.ts
+++ src/app/layouts/dashboard-layout/dashboard-layout.ts
@@ -2,23 +2,37 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Sidebar } from '../../shared/components/sidebar/sidebar';
 import { ThemeService } from '../../core/services/themeService/theme-service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard-layout',
-  imports: [RouterOutlet, Sidebar],
+  imports: [RouterOutlet, Sidebar, CommonModule],
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.css',
 })
 export class DashboardLayout {
   constructor(public theme: ThemeService) {}
-    isCollapsed = signal(JSON.parse(localStorage.getItem('sideBarCollabse')!));
-
-  get themeSignal() {
-    return this.theme.theme;
-  }
+  private initialCollapse = (() => {
+    try {
+      const raw = localStorage.getItem('sideBarCollabse');
+      return raw ? JSON.parse(raw) : false;
+    } catch {
+      return false;
+    }
+  })();
+  isCollapsed = signal<boolean>(this.initialCollapse);
 
   toggleSidebar() {
-    this.isCollapsed.update(v => !v);
-    localStorage.setItem('sideBarCollabse', JSON.stringify(this.isCollapsed()))
+    this.isCollapsed.update((v) => !v);
+    localStorage.setItem('sideBarCollabse', JSON.stringify(this.isCollapsed()));
+  }
+
+  isMobileOpen = signal(true);
+
+  toggleMobileSidebar() {
+    this.isMobileOpen.update((v) => !v);
+  }
+  closeMobileSidebar() {
+    this.isMobileOpen.set(false);
   }
 }
